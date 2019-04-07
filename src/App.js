c81bd847e281bba71b146de0c2ae11e08fe20b18
Cyrus Cowley <cyficowley@gmail.com>
2019-04-07 16:53:29 -0700

    initial key listening

--- src/App.js
+++ src/App.js
@@ -4,39 +4,41 @@ import './App.css';
 import Joystick from 'react-joystick';
  
 const joyOptions = {
-    mode: 'semi',
-    catchDistance: 150,
-    color: 'white'
+  mode: 'static',
+  position: {top: 0, left: 0},
+  catchDistance: 150,
+  color: 'white'
 }
  
 const containerStyle = {
-    position: 'relative',
-    height: '350px',
-    width: '100%',
-    background: 'linear-gradient(to right, #E684AE, #79CBCA, #77A1D3)'
+  position: 'static',
+  height: '350px',
+  width: '100%',
+  background: 'linear-gradient(to right, #E684AE, #79CBCA, #77A1D3)'
 } 
 
 class App extends Component {
   constructor() {
-      super();
-      this.managerListener = this.managerListener.bind(this);
+    super();
+    this.managerListener = this.managerListener.bind(this);
+    this.state = {angle:0, force:0}
+    
   }
  
+  handleKeyPress = (event) => {
+    console.log('enter press here! ')
+  }
+
   managerListener(manager) {
-      manager.on('move', (e, stick) => {
-          console.log('I moved!')
-      })
-      manager.on('end', () => {
-          console.log('I ended!')
-      })
+    manager.on('move', (e, stick) => {
+      this.setState({angle:stick.angle.radian, force:stick.force})
+    })
   }
 
   render() {
     return (
-      <div>
-
+      <div onKeyPress={this.handleKeyPress} tabIndex="0">
         <Joystick joyOptions={joyOptions} containerStyle={containerStyle} managerListener={this.managerListener} />
-        
       </div>
     );
   }
